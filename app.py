4613bdc810edab0cd724693de08b89625f2e1452
zf81 <111600840+zf81@users.noreply.github.com>
2022-12-13 01:22:40 -0500

    /

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return '<h1>Hello World!</h1>'
-
-@app.route('/page1')
-def page1():
-    return '<h1>Page 1</h1>'
+def homepage():
+    return render_template('flask-page1.html')
 
 @app.route('/page2')
-def page2():
-    return '<h1>Page 2</h1>'
+def homepage_styles():
+    return render_template('flask-page2.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
+
